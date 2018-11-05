5733590aa65405fd0c89c9d5b0508b387524f21e
nanziyu <523939784@qq.com>
2018-11-05 17:14:11 +0800

    新加类

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -35,6 +35,7 @@ public class LicenseServiceController {
 
     @RequestMapping(value="{licenseId}",method = RequestMethod.POST)
     public String saveLicenses( @PathVariable("licenseId") String licenseId) {
+        System.out.print(1111);
         return String.format("This is the add post");
     }
 
